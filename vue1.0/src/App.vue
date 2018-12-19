a6558bb655af45346584e998aafe01cdf8630b1b
avalonwb <avalonwb@qq.com>
2018-12-19 21:48:03 +0800

    1.0

---
+++ vue1.0/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {}
+
+</script>
+
+<style lang="less">
+
+  *{
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+
+  body,html {
+    width: 100%;
+    width: 100%;
+
+    #app {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+</style>
---
+++ vue1.0/src/components/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-form ref="form" :model="form" label-width="80px">
+    <el-form-item label="用户名">
+      <el-input v-model="form.username"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input v-model="form.password"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">确定</el-button>
+      <el-button>取消</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
+}
+</script>
+
+<style lang="less">
+
+.login {
+  height: 100%;
+  width: 100%;
+  background-color:  #2d434c;
+  overflow: hidden;
+  .el-form {
+    width: 400px;
+    background-color: #fff;
+    margin: 200px auto;
+    padding: 75px 40px 15px;
+    position: relative;
+  }
+}
+
+</style>
