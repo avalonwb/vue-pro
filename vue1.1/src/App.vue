c52e7675f54f3c234150ae53e2e57eecaf3c5da5
avalonwb <avalonwb@qq.com>
2018-12-20 10:53:44 +0800

    1.1

---
+++ vue1.1/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+  body,html {
+    width: 100%;
+    height: 100%;
+
+    #app {
+      width: 100%;
+      height: 100%;
+    }
+  }
+</style>
---
+++ vue1.1/src/components/Login.vue
@@ -0,0 +1,67 @@
+<template>
+
+  <div class="login">
+
+    <el-form ref="form" :model="form" label-width="80px">
+      <img src="../assets/author.jpg" alt="">
+      <el-form-item label="用户名">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">确定</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.login {
+  width: 100%;
+  height: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
+
+  .el-form {
+    width: 400px;
+    padding: 75px 40px 15px 40px;
+    margin: 200px auto;
+    border-radius: 20px;
+    position: relative;
+    background-color: #fff;
+
+    img {
+      position: absolute;
+      top: -80px;
+      left: 50%;
+      transform: translateX(-50%);
+      border-radius: 50%;
+      border: 10px solid #fff;
+    }
+  }
+}
+</style>
